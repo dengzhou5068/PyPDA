9ca74f0426e28c56ce721bd4b69759b021bbd617
dengzhou5068 <77594204+dengzhou5068@users.noreply.github.com>
2026-06-15 10:57:02 +0800

    Update __init__.py

--- opentargets/__init__.py
+++ opentargets/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-OpenTargets数据处理模块
+OpenTargets模块初始化文件
 """
+
+from .opentargets_api import OpenTargetsAPI
+from .opentargets_processor import OpenTargetsProcessor
+
+__all__ = ['OpenTargetsAPI', 'OpenTargetsProcessor']
